234dc5b6b865a6eb7aff8a614218938bf80b781e
shixan <114354917+shixan@users.noreply.github.com>
2025-06-14 17:09:49 +0300

    Add files via upload

---
+++ spring-user-service/src/main/java/com/example/springuserservice/SpringUserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.springuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserServiceApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringUserServiceApplication.class, args);
+    }
+}
